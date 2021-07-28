cc90e257a213ef0e142b11dbbff7189f3ee197d8
Jay Lee <88051502+Jay-clyh-Lee@users.noreply.github.com>
2021-07-28 11:13:29 -0700

    Update accepted_subs.py

--- accepted_subs.py
+++ accepted_subs.py
@@ -16,4 +16,19 @@ def canConstruct(ransomNote: str, magazine: str):
 
 #------
 
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: intz
+        """
+        #self.nums = nums
+        i = 0
+        for j in range(len(nums)):
+            if i < 1 or nums[j] != nums[j - 1]: # if 1st != last/previous
+                nums[i] = nums[j]
+                i += 1
+        #print(nums)
+        return i
