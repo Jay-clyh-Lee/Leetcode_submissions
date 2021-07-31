c87e5fd657667a39fdb1c35aee1b85a11ad70d9c
Jay Lee <88051502+Jay-clyh-Lee@users.noreply.github.com>
2021-07-31 13:00:26 -0700

    Update accepted_subs.py

--- accepted_subs.py
+++ accepted_subs.py
@@ -18,17 +18,40 @@ def canConstruct(ransomNote: str, magazine: str):
 
 # https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
-class Solution(object):
-    def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: intz
-        """
-        #self.nums = nums
-        i = 0
-        for j in range(len(nums)):
-            if i < 1 or nums[j] != nums[j - 1]: # if 1st != last/previous
-                nums[i] = nums[j]
-                i += 1
-        #print(nums)
-        return i
+def removeDuplicates(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: intz
+    """
+    #self.nums = nums
+    i = 0
+    for j in range(len(nums)):
+        if i < 1 or nums[j] != nums[j - 1]: # if 1st != last/previous
+            nums[i] = nums[j]
+            i += 1
+    #print(nums)
+    return i
+
+  
+#------
+
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+# runtime: 68 ms
+def twoSum(self, numbers: List[int], target: int) -> List[int]:        
+  # two pointer
+  l, r = 0, len(numbers)-1
+
+  while l < r:
+    s = numbers[l] + numbers[r]
+    if numbers[l] + numbers[r] == target:
+        return [l+1, r+1]
+
+    # take advantage of the fact that numbers is an ordered list
+
+    # if s is smaller, next number in the list will make s bigger
+    elif s < target:
+        l += 1
+
+    # if l is bigger, previous number in the list will make s smaller 
+    else:
+        r -= 1
