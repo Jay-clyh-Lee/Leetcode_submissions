2f5935249563f29fbad834d9f873dfddc5541b82
Jay Lee <88051502+Jay-clyh-Lee@users.noreply.github.com>
2021-08-05 12:14:40 -0700

    Update accepted_subs.py

--- accepted_subs.py
+++ accepted_subs.py
@@ -215,7 +215,16 @@ class Solution:
                 
         return len(set([int(x) for x in string.split()]))
         
-        
+# alternative using map()
+# runtime: 28ms
+# memory: 14.3 MB
+
+class Solution:
+    def numDifferentIntegers(self, word: str) -> int:
+        s = ''.join(c if c.isdigit() else ' ' for c in word)
+        return len(set(map(int, s.split())))
+
+
 #-------
 
 # https:
