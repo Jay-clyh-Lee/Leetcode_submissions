4085ce14274887655f8450e7226e84c55202205a
Jay Lee <88051502+Jay-clyh-Lee@users.noreply.github.com>
2021-08-12 08:21:35 -0700

    Update accepted_subs.py

--- accepted_subs.py
+++ accepted_subs.py
@@ -296,8 +296,17 @@ class Solution:
 #-------
 
 # https:
-# runtime:
-# memory:
+# runtime: 56ms
+# memory: 14.2 MB
+
+class Solution:
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
+        l = r = start
+        while l >= 0 or r < len(nums):
+            if l >= 0 and nums[l] == target: return start - l
+            if r < len(nums) and nums[r] == target: return r - start
+            l -= 1
+            r += 1
 
 #-------
 
