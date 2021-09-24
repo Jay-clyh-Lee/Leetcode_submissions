110929d0da58b1f962e574af0d167cd91019796b
Jay Lee <88051502+Jay-clyh-Lee@users.noreply.github.com>
2021-09-23 18:18:40 -0700

    Update accepted_subs.py

--- accepted_subs.py
+++ accepted_subs.py
@@ -479,13 +479,33 @@ class Solution:
         except:
             return False
 
+        
 #-------
 
-# https:
-# runtime:
-# memory:
+# https://leetcode.com/problems/integer-to-roman/
+# runtime: 52ms
+# memory: 14.4 MB
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        values = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ]
+        numerals = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" ]
+        res, i = "", 0
+        while num:
+            res += (num//values[i]) * numerals[i]
+            num %= values[i]
+            i += 1
+        return res
 
+    def intToRoman(self, num):
+        values = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ]
+        numerals = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" ]
+        res = ""
+        for i, v in enumerate(values):
+            res += (num//v) * numerals[i]
+            num %= v
+        return res
 
+    
 #-------
 
 # https:
